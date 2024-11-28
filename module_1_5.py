d5e8d01721d3e3f9a5fa017ba0c84649b4bdf875
ramil76-art <gulynara11@gmail.com>
2024-11-28 20:00:11 +0300

    dz 5

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var=[2,3,'r','a']
+print(immutable_var)#значение кортеджа в ковычках не изменяемые
+print(immutable_var[3])
+immutable_var[3]='telega'
+print(immutable_var)
+mutable_list=[1,2,3,'p']
+print(mutable_list[3])
+mutable_list[3]='ramil'
+print(mutable_list)
