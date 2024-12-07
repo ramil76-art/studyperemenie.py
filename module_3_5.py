e1002ffc426ebc7701eb14e69592d1a51d053d5e
ramil76-art <gulynara11@gmail.com>
2024-12-07 21:38:26 +0300

    dz 5

--- module_3_5.py
+++ module_3_5.py
@@ -1,13 +1,14 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    if len(str_number) > 1:
-        first = int(str_number[0])
+    str_number = str_number.replace("0", "1")
+    first = int(str_number[0])
+    if len(str_number) > 1 and first != 0:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return int(str_number)
+        return first
 
 
 result = get_multiplied_digits(40203)
 print(result)
-result_2 = get_multiplied_digits(402030)
-print(result_2)
+result2 = get_multiplied_digits(402030)
+print(result2)
