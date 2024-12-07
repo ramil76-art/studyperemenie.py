a5240f519fb3c14bfaca4fc1e8e70b9bd09a5df9
ramil76-art <gulynara11@gmail.com>
2024-12-07 10:30:18 +0300

    dz 5

---
+++ module_34.py
@@ -0,0 +1,16 @@
+def single_root_words(root_word ="richiest", *other_words):
+    same_words=[]
+    root_word_lower = root_word.lower() #перевод в нижний регистор
+    for i in other_words:
+        other_words_lower = i.lower()
+        if root_word_lower in other_words_lower or other_words_lower in root_word_lower:
+            same_words.append(i)
+
+
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
--- module_3_2.py
+++ module_3_2.py
@@ -1,20 +1,24 @@
-# создаём функцию send_email
-def send_email(messadje,recipient,sender = "univerity.help@gmail.com"):
-    if("@"and(".com"or".ru"or".net")) not in (recipient or sender) or("@" or(".com"or"ru"or".net")) not in (recipient or sender):
-        # создаём  1 условие
-        print(f"Невозможно отправить письмо с адреса {sender}на адрес {recipient}")
-    elif recipient == sender:
-        # создаём 2 условие
-        print("Нельзя отправить письмо самому себе!")
-        # создаём 3 условие
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса{sender} на адрес {recipient}.")
-        # создаём 4 условие
-    elif sender !="university.help@gmail.com":
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-        # вывод результата
-        send_email('messagje','university.help@mail.com','@.com')
-        send_email('messagje','university.help@mail.com')
-        send_email('messagje','universi@.com')
-        send_email('messagje','universi@.co')
-        send_email('messagje','universi@.com')
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
+    a = '@'
+    while 1 > 0:
+        if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
+            if a not in recipient and sender:
+                print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+                break
+        else:
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+            break
+        if sender == recipient:
+            print('Нельзя отправить письмо самому себе!')
+            break
+        elif sender == 'university.help@gmail.com':
+            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+            break
+        elif sender != 'university.help@gmail.com':
+            print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+            break
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
