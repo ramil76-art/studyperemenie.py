fc0a18cbab19b9fc0bf7d58dedf94c2b6851476c
ramil76-art <gulynara11@gmail.com>
2024-12-12 20:50:19 +0300

    dz 5

--- fake_mafh.py
+++ fake_mafh.py
@@ -1,8 +1,6 @@
-from math import inf
-
 def divide(first, second):
     if second != 0:
         f = first/second
         return f
     else:
-        return inf
+        return'Ошибка'
---
+++ module_4_1.py
@@ -0,0 +1,14 @@
+from fake_mafh import divide as fake_divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+print(result1)
+print(result2)
+
+from true_math import divide as true_divide
+
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+print(result3)
+print(result4)
